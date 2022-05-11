e8ae086e2f49633a5e53459bd3637962f5e86be0
Shoma <shoma.gujjar@gmail.com>
2022-05-11 23:09:20 +0100

    Code refactor

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Xml;
 using System.Xml.Serialization;
 using Brady_GenerationReport;
 using Brady_GenerationReport.Input;
 using Brady_GenerationReport.OutPut;
+using Brady_GenerationReport.Utilities;
 using Day = Brady_GenerationReport.Output.Day;
 
 Console.WriteLine("Hello, Brady!");
@@ -36,31 +36,22 @@ static void MonitorDirectory(ConfigLocations? path)
 
 static void FileSystemWatcherCreated(object sender, FileSystemEventArgs e)
 {
-    Console.WriteLine("File created: {0}", e.Name);
-    var report = PerformParsingOfTheXmlFile(e.FullPath);
+    var fileDrop = e.Name;
+    Console.WriteLine("File created: {0}", fileDrop);
+    var report = ParsingGenerationReport(e.FullPath);
     //once valid xml file is found perform calculation and produce out put file in xml format
-    DataProcessing(report, e.Name);
-    Console.WriteLine("XML file parsed");
+    DataProcessing(report, fileDrop);
 }
 
-static GenerationReport PerformParsingOfTheXmlFile(string path)
+static GenerationReport ParsingGenerationReport(string path)
 {
-    XmlReader reader = XmlReader.Create(path);
-    XmlSerializer xmlSerializer = new XmlSerializer(typeof(GenerationReport));
-    var generationReport = (GenerationReport)xmlSerializer.Deserialize(reader);
+    var generationReport = Helper.ParseXMLFile<GenerationReport>(path);
     return generationReport;
 }
 
-static void DataProcessing(GenerationReport report, string inputFileName)
+static List<Generator> GetWindGenerators(List<WindGenerator> windGenerators, ValueFactor valueFactor)
 {
-    //Create output xml file
-    List<Generator> generators = new List<Generator>();
-    var referenceData = ReadReferenceDataFile();
-
-    //Wind
-    var valueFactor = referenceData.Factors.ValueFactor;
-    var emissionFactor = referenceData.Factors.EmissionsFactor;
-    var windGenerators = report.Wind.WindGenerators;
+    var generators = new List<Generator>();
     foreach (var windGenerator in windGenerators)
     {
         var days = windGenerator.Generation.Days;
@@ -74,84 +65,95 @@ static void DataProcessing(GenerationReport report, string inputFileName)
         }
         generators.Add(new Generator { Name = windGeneratorName, Total = totalWindGenerationValue });
     }
+    return generators;
+}
 
-    var outPutDays = new List<Day>();
-
-    //Gas
-    var gasGenerator = report.Gas.GasGenerator;
+static Generator GetGasGenerator(GasGenerator gasGenerator, ValueFactor valueFactor,
+    EmissionsFactor emissionFactor, ref List<Day> outputDays)
+{
     var emissionRating = gasGenerator.EmissionsRating;
     double totalGasGenerationValue = 0.0;
     foreach (var day in gasGenerator.Generation.Days)
     {
         totalGasGenerationValue += day.Energy * day.Price * valueFactor.Medium;
         var emission = day.Energy * emissionRating * emissionFactor.Medium;
-        outPutDays.Add(new Day
+        outputDays.Add(new Day
         {
             Date = day.Date,
             Emission = emission,
             Name = gasGenerator.Name
         });
     }
-    generators.Add(new Generator { Name = gasGenerator.Name, Total = totalGasGenerationValue });
+    return new Generator { Name = gasGenerator.Name, Total = totalGasGenerationValue };
+}
 
-    //Coal
-    var coalGenerator = report.Coal.CoalGenerator;
+static Generator GetCoalGenerator(CoalGenerator coalGenerator, ValueFactor valueFactor,
+    EmissionsFactor emissionFactor, ref List<Day> outputDays)
+{
     var coalEmissionRating = coalGenerator.EmissionsRating;
     double totalCoalGenerationValue = 0.0;
     foreach (var day in coalGenerator.Generation.Days)
     {
         totalCoalGenerationValue += day.Energy * day.Price * valueFactor.Medium;
         var coalEmission = day.Energy * coalEmissionRating * emissionFactor.High;
-        outPutDays.Add(new Day
+        outputDays.Add(new Day
         {
             Date = day.Date,
             Emission = coalEmission,
             Name = coalGenerator.Name
         });
     }
-    generators.Add(new Generator { Name = coalGenerator.Name, Total = totalCoalGenerationValue });
+    return new Generator { Name = coalGenerator.Name, Total = totalCoalGenerationValue };
+}
+
+static ReferenceData ParseReferenceDataFile()
+{
+    var path = AppsettingReader.ReadSection<ConfigLocations>("ConfigLocations");
+    string filename = $"{path.ReferenceData}\\ReferenceData.xml";
+    var referenceData = Helper.ParseXMLFile<ReferenceData>(filename);
+    return referenceData;
+}
+
+static void DataProcessing(GenerationReport report, string inputFileName)
+{
+    //Create output xml file
+    List<Generator> generators = new List<Generator>();
+    var referenceData = ParseReferenceDataFile();
+    var valueFactor = referenceData.Factors.ValueFactor;
+    var emissionFactor = referenceData.Factors.EmissionsFactor;
+
+    //Wind
+    var windGenerators = GetWindGenerators(report.Wind.WindGenerators, valueFactor);
+    generators.AddRange(windGenerators);
+
+    //Gas
+    var outPutDays = new List<Day>();
+    var gasGenerator = GetGasGenerator(report.Gas.GasGenerator, valueFactor, emissionFactor, ref outPutDays);
+    generators.Add(gasGenerator);
+
+    //Coal
+    var coalGenerator = GetCoalGenerator(report.Coal.CoalGenerator, valueFactor, emissionFactor, ref outPutDays);
+    generators.Add(coalGenerator);
 
     //Calculate Actual Heat Rate = TotalHeatInput / ActualNetGeneration
-    var actualHeatRateValue = coalGenerator.TotalHeatInput / coalGenerator.ActualNetGeneration;
-    ActualHeatRate actualHeatRate = new ActualHeatRate()
-    {
-        Name = coalGenerator.Name,
-        HeatRate = actualHeatRateValue
-    };
+    var actualHeatRateValue = report.Coal.CoalGenerator.TotalHeatInput / report.Coal.CoalGenerator.ActualNetGeneration;
 
-    Totals totals = new Totals()
-    {
-        Generator = generators
-    };
+    var actualHeatRate = new ActualHeatRate { Name = coalGenerator.Name, HeatRate = actualHeatRateValue };
 
-    MaxEmissionGenerators maxEmissionGenerators = new MaxEmissionGenerators()
-    {
-        Day = outPutDays
-    };
+    var totals = new Totals { Generator = generators };
 
-    ActualHeatRates actualHeatRates = new ActualHeatRates()
-    {
-        ActualHeatRate = actualHeatRate
-    };
+    var maxEmissionGenerators = new MaxEmissionGenerators { Day = outPutDays };
 
-    GenerationOutput output = new GenerationOutput()
+    var actualHeatRates = new ActualHeatRates { ActualHeatRate = actualHeatRate };
+
+    var generationOutput = new GenerationOutput
     {
         Totals = totals,
         MaxEmissionGenerators = maxEmissionGenerators,
         ActualHeatRates = actualHeatRates
     };
-    //CreateOutPutFile
-    CreateOutputXmlFile(inputFileName, output);
-}
 
-static ReferenceData ReadReferenceDataFile()
-{
-    var path = AppsettingReader.ReadSection<ConfigLocations>("ConfigLocations");
-    string filename = $"{path.ReferenceData}\\ReferenceData.xml";
-    XmlReader reader = XmlReader.Create(filename);
-    XmlSerializer xmlSerializer = new XmlSerializer(typeof(ReferenceData));
-    ReferenceData referenceData = (ReferenceData)xmlSerializer.Deserialize(reader);
-    return referenceData;
+    CreateOutputXmlFile(inputFileName, generationOutput);
 }
 
 static void CreateOutputXmlFile(string inputFileName, GenerationOutput output)
---
+++ Utilities/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace Brady_GenerationReport.Utilities
+{
+    public static class Helper
+    {
+        public static T ParseXMLFile<T>(string path)
+        {
+            var reader = XmlReader.Create(path);
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            T data = (T)xmlSerializer.Deserialize(reader);
+            return data;
+        }
+    }
+}
